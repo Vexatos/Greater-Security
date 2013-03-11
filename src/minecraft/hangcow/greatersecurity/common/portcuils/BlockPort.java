c4e20558b240eff74104bfedc68639c6970c68c7
RSeifert <rseifert.phone@gmail.com>
2013-03-10 22:38:41 -0400

    worked on portcuils

--- src/minecraft/hangcow/greatersecurity/common/portcuils/BlockPort.java
+++ src/minecraft/hangcow/greatersecurity/common/portcuils/BlockPort.java
@@ -1,25 +1,47 @@
 package hangcow.greatersecurity.common.portcuils;
 
+import universalelectricity.prefab.BlockMachine;
 import net.minecraft.block.BlockContainer;
 import net.minecraft.block.material.Material;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.world.World;
 
-public class BlockPort extends BlockContainer {
+public class BlockPort extends BlockMachine
+{
 
-	protected BlockPort(int par1) {
+	protected BlockPort(int par1)
+	{
 		super(par1, Material.rock);
+		this.setCreativeTab(CreativeTabs.tabRedstone);
+		this.setBlockName("port");
 	}
 
 	@Override
-	public TileEntity createNewTileEntity(World var1) 
+	public boolean onMachineActivated(World par1World, int x, int y, int z, EntityPlayer par5EntityPlayer, int side, float hitX, float hitY, float hitZ)
+	{
+		// TODO open gui
+		return false;
+	}
+
+	@Override
+	public boolean onSneakUseWrench(World par1World, int x, int y, int z, EntityPlayer par5EntityPlayer, int side, float hitX, float hitY, float hitZ)
+	{
+		// TODO rotate to face the player
+		return false;
+	}
+
+	@Override
+	public TileEntity createNewTileEntity(World var1)
 	{
 		return null;
 	}
+
 	@Override
-	public TileEntity createNewTileEntity(World var1, int meta) 
+	public TileEntity createNewTileEntity(World var1, int meta)
 	{
-		return null;
+		return new TileEntityPort();
 	}
 
 }
